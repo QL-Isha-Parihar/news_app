f39823d9eb768c68ce9894f125cdaa41c7ecf558
apple <prateek.maurya@quokkalabs.com>
2026-08-08 08:00:23 +0530

    Fix : fixed issues

--- lib/widgets/article_card.dart
+++ lib/widgets/article_card.dart
@@ -68,7 +68,8 @@ class ArticleCard extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      article.title,                   maxLines: 3,
+                      isSecondTab ? article.title.substring(0, 1000) : article.title,
+                      maxLines: 3,
                       overflow: TextOverflow.ellipsis,
                       style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
                     ),
